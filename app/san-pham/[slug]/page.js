befa1843b5df1947537855d30265efc3604d0ddc
spaghetti868 <nguyenbahieu0712@gmail.com>
2026-05-27 19:23:06 +0700

    fix path import

--- app/san-pham/[slug]/page.js
+++ app/san-pham/[slug]/page.js
@@ -1,5 +1,5 @@
 import { collection, query, where, getDocs } from 'firebase/firestore'
-import { db } from '@/lib/firebase'
+import { db } from '../../../lib/firebase'
 import { notFound } from 'next/navigation'
 
 const fmt = n => new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(n || 0)
